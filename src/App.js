b2b31098ff8aa4e8bedeb6e24458014a000edeaa
Paola Persico <32850381+paolapersico1@users.noreply.github.com>
2024-02-07 19:49:15 +0100

    Change to SPA

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Preloader from "../src/components/Pre";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home/Home";
 import About from "./components/About/About";
+import Skills from "./components/Skills/Skills";
 import Projects from "./components/Projects/Projects";
 import Footer from "./components/Footer";
 import Resume from "./components/Resume/ResumeNew";
@@ -16,6 +17,8 @@ import ScrollToTop from "./components/ScrollToTop";
 import "./style.css";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import CustomCursor from 'custom-cursor-react';
+import 'custom-cursor-react/dist/index.css';
 
 function App() {
   const [load, upadateLoad] = useState(true);
@@ -32,15 +35,28 @@ function App() {
     <Router>
       <Preloader load={load} />
       <div className="App" id={load ? "no-scroll" : "scroll"}>
+        <CustomCursor
+          targets={['a']}
+          customClass='custom-cursor'
+          dimensions={50}
+          fill='#FFF'
+          strokeColor='#ff7328'
+          targetScale={2}
+          strokeWidth={15}
+          smoothness={{
+            movement: 0.2,
+            scale: 0.1,
+            opacity: 0.2,
+          }}
+          targetOpacity={0.5}
+        />
         <Navbar />
         <ScrollToTop />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/project" element={<Projects />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/resume" element={<Resume />} />
-          <Route path="*" element={<Navigate to="/"/>} />
-        </Routes>
+        <Home />
+        <About />
+        <Skills />
+        <Projects />
+        <Resume />
         <Footer />
       </div>
     </Router>
