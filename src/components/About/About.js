2e32ae0358311b05693bc4618bdd96184e20008b
Paola Persico <32850381+paolapersico1@users.noreply.github.com>
2024-02-12 21:46:39 +0100

    Change summary

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -10,40 +10,19 @@ function About() {
         <Row>
           <Col md={8} className="home-about-description">
             <h1 style={{ fontSize: "2.6em" }}>
-              LET ME <span className="purple"> INTRODUCE </span> MYSELF
+              LET ME <span className="orange"> INTRODUCE </span> MYSELF
             </h1>
             <p className="home-about-body">
-              I fell in love with programming and I have at least learnt
-              something, I think… 🤷‍♂️
-              <br />
-              <br />I am fluent in classics like
-              <i>
-                <b className="purple"> C++, Javascript and Go. </b>
-              </i>
-              <br />
-              <br />
-              My field of Interest's are building new &nbsp;
-              <i>
-                <b className="purple">Web Technologies and Products </b> and
-                also in areas related to{" "}
-                <b className="purple">
-                  Blockchain.
-                </b>
-              </i>
-              <br />
-              <br />
-              Whenever possible, I also apply my passion for developing products
-              with <b className="purple">Node.js</b> and
-              <i>
-                <b className="purple">
-                  {" "}
-                  Modern Javascript Library and Frameworks
-                </b>
-              </i>
-              &nbsp; like
-              <i>
-                <b className="purple"> React.js and Next.js</b>
-              </i>
+              I am a software developer based in Turin, Italy. 
+              <br></br><br></br>
+              I have always loved technology, so I studied <b className="orange">Computer Science</b> achieving a Master's Degree with full marks at University Alma Mater of Bologna. 
+              <br></br><br></br>
+              During my academic years, I have worked on 20+ (group and invidual) projects, learning a great number of programming languages, such as C, Java and Python, and exploring novel fields such as Machine Learning, VR/AR and Context-Aware Systems.
+              <br></br><br></br>
+              My professional experience began with an Erasmus Traineeship in UK at Global Voices Ltd and continued with an internship in Bologna at TicketSms Srl; in both companies I was mostly involved in <b className="orange">Back-end Web Development</b> in PHP. 
+   
+              <br></br><br></br>
+              In my spare time, I like tinkering with Home Assistant, playing videogames and trekking.
             </p>
           </Col>
           <Col md={4} className="myAvtar">
