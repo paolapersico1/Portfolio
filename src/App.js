42a5be4085d6ef2a341605f1cc0ad9051f488116
Paola Persico <32850381+paolapersico1@users.noreply.github.com>
2024-02-12 21:47:12 +0100

    Update dependencies

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from "react";
+import { initParticlesEngine } from "@tsparticles/react";
+import { loadSlim } from "@tsparticles/slim";
 import Preloader from "../src/components/Pre";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home/Home";
@@ -6,50 +8,32 @@ import About from "./components/About/About";
 import Skills from "./components/Skills/Skills";
 import Projects from "./components/Projects/Projects";
 import Footer from "./components/Footer";
-import Resume from "./components/Resume/ResumeNew";
+import Resume from "./components/Resume/Resume";
 import {
   BrowserRouter as Router,
-  Route,
-  Routes,
-  Navigate
 } from "react-router-dom";
 import ScrollToTop from "./components/ScrollToTop";
 import "./style.css";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import CustomCursor from 'custom-cursor-react';
-import 'custom-cursor-react/dist/index.css';
 
 function App() {
-  const [load, upadateLoad] = useState(true);
+  const [load, updateLoad] = useState(true);
 
   useEffect(() => {
-    const timer = setTimeout(() => {
-      upadateLoad(false);
-    }, 1200);
-
-    return () => clearTimeout(timer);
+    initParticlesEngine(async (engine) => {
+      await loadSlim(engine);
+    }).then(() => {
+      setTimeout(() => {
+        updateLoad(false);
+      }, 1200);
+    });
   }, []);
 
   return (
     <Router>
       <Preloader load={load} />
       <div className="App" id={load ? "no-scroll" : "scroll"}>
-        <CustomCursor
-          targets={['a']}
-          customClass='custom-cursor'
-          dimensions={50}
-          fill='#FFF'
-          strokeColor='#ff7328'
-          targetScale={2}
-          strokeWidth={15}
-          smoothness={{
-            movement: 0.2,
-            scale: 0.1,
-            opacity: 0.2,
-          }}
-          targetOpacity={0.5}
-        />
         <Navbar />
         <ScrollToTop />
         <Home />
